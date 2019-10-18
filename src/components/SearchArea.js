e4d1a28b930ea64bfcd5d8dc880e5c6f0142780f
Ladarius Greene <lg@Ladariuss-MacBook-Pro.local>
2019-10-18 10:21:25 -0700

    first commit

---
+++ src/components/SearchArea.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SearchArea = (props) => {
+    return (
+        <div className="container">
+            <div className='row'>
+                <section className="col s4 offset-4">
+                    <form action="" onSubmit={props.handleSubmit}>
+                        <div className="input-field">
+                            <input placeholder="Search Movie..." type="text"  onChange={props.handleChange}
+                            />
+                        </div>
+                    </form>
+                </section>
+            </div>
+        </div>
+    )
+    
+}
+
+export default SearchArea;
